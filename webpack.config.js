95c6436c62cfb488b23b261d297de7b368cb484e
Wendy Dherin <wendydherin@gmail.com>
2018-08-18 10:08:18 +0100

    Specify output path and filename, add helpful comments to explain how it works.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
+// the 'path' module is part of node.js;
+// when we run webpack, it runs in the node.js environment,
+// so we can make use of any piece of node.js technology we like
+// inside of our tooling pipeline
+const path = require('path')
+
 const config = {
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    // webpack-dev requires that output.path is an absolute path
+    // so that's why we are using the resolve function of the path module.
+    // Here, the resolve function saves the bundle.js file into a newly created
+    // 'dist' directory that's inserted into current directory (specified by '__dirname')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
 }
 
 module.exports = config
